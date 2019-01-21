308225c127abc72c09ab09ad1df71e21bfc099ea
yakubenko <ayakubenko92@gmail.com>
2019-01-21 17:25:31 +0200

    need test

--- app/src/main/java/com/taxi/optio/profitable/proxi/main/ui/MainFragment.kt
+++ app/src/main/java/com/taxi/optio/profitable/proxi/main/ui/MainFragment.kt
@@ -3,18 +3,11 @@ package com.taxi.optio.profitable.proxi.main.ui
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.os.Handler
-import android.widget.EditText
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView
-import androidx.appcompat.widget.AppCompatEditText
 import br.com.simplepass.loadingbutton.customViews.CircularProgressImageButton
 import com.jakewharton.rxbinding2.widget.RxTextView
-import com.jakewharton.rxbinding2.widget.textChanges
 import com.taxi.optio.profitable.proxi.R
 import com.taxi.optio.profitable.proxi.base.BaseFragment
-import com.taxi.optio.profitable.proxi.utils.extentions.onTextChanged
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 class MainFragment : BaseFragment() {
@@ -22,8 +15,8 @@ class MainFragment : BaseFragment() {
     override val layout: Int = R.layout.fr_main
     private var btnFind: CircularProgressImageButton? = null
 
-    private var startPoint: AppCompatEditText? = null
-    private var endPoint: AppCompatEditText? = null
+    private var startPoint: AppCompatAutoCompleteTextView? = null
+    private var endPoint: AppCompatAutoCompleteTextView? = null
 
     companion object {
         fun newInstance() = MainFragment()
@@ -37,13 +30,15 @@ class MainFragment : BaseFragment() {
 
         startPoint = view?.findViewById(R.id.startPoint)
 
-        startPoint!!.textChanges()
-            .debounce(300, TimeUnit.MILLISECONDS)
-            .distinctUntilChanged()
-            .filter { source -> source.toString().isNotBlank() }
-            .flatMap { source -> viewModel.onStartPointTextChanged(source.toString()) }
-            .subscribe({ result -> println("test " + result) })
-
+        startPoint?.let {
+            RxTextView.textChanges(it)
+                .debounce(300, TimeUnit.MILLISECONDS)
+                .map { charSequence ->  charSequence.toString() }
+                .distinctUntilChanged()
+                .filter { source -> source.isNotBlank() }
+                .flatMap { source -> viewModel.onStartPointTextChanged(source) }
+                .subscribe({ result -> println("test " + result) })
+        }
 
         btnFind = view?.findViewById(R.id.btnFind)
         btnFind?.setOnClickListener {
