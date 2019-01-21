0fed14795ce4b6bde190cda2ac34ba92ad1ec6e9
yakubenko <ayakubenko92@gmail.com>
2019-01-21 17:11:44 +0200

    need test

--- app/src/main/java/com/taxi/optio/profitable/proxi/main/ui/MainFragment.kt
+++ app/src/main/java/com/taxi/optio/profitable/proxi/main/ui/MainFragment.kt
@@ -3,20 +3,27 @@ package com.taxi.optio.profitable.proxi.main.ui
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.os.Handler
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView
+import androidx.appcompat.widget.AppCompatEditText
 import br.com.simplepass.loadingbutton.customViews.CircularProgressImageButton
+import com.jakewharton.rxbinding2.widget.RxTextView
+import com.jakewharton.rxbinding2.widget.textChanges
 import com.taxi.optio.profitable.proxi.R
 import com.taxi.optio.profitable.proxi.base.BaseFragment
 import com.taxi.optio.profitable.proxi.utils.extentions.onTextChanged
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-class MainFragment : BaseFragment(){
+class MainFragment : BaseFragment() {
 
     override val layout: Int = R.layout.fr_main
     private var btnFind: CircularProgressImageButton? = null
 
-    private var startPoint: AppCompatAutoCompleteTextView? = null
-    private var endPoint: AppCompatAutoCompleteTextView? = null
+    private var startPoint: AppCompatEditText? = null
+    private var endPoint: AppCompatEditText? = null
 
     companion object {
         fun newInstance() = MainFragment()
@@ -29,7 +36,14 @@ class MainFragment : BaseFragment(){
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
         startPoint = view?.findViewById(R.id.startPoint)
-        startPoint?.onTextChanged { viewModel.onStartPointTextChanged(it) }
+
+        startPoint!!.textChanges()
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .filter { source -> source.toString().isNotBlank() }
+            .flatMap { source -> viewModel.onStartPointTextChanged(source.toString()) }
+            .subscribe({ result -> println("test " + result) })
+
 
         btnFind = view?.findViewById(R.id.btnFind)
         btnFind?.setOnClickListener {
--- app/src/main/java/com/taxi/optio/profitable/proxi/main/ui/MainViewModel.kt
+++ app/src/main/java/com/taxi/optio/profitable/proxi/main/ui/MainViewModel.kt
@@ -1,29 +1,21 @@
 package com.taxi.optio.profitable.proxi.main.ui
 
 import androidx.lifecycle.ViewModel
+import com.taxi.optio.profitable.proxi.main.domain.entity.PlacesModel
 import com.taxi.optio.profitable.proxi.main.domain.interactors.PriceListInteractor
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
-import java.util.concurrent.TimeUnit
 
 class MainViewModel: ViewModel(), KoinComponent {
 
     private var startPointObservable: Disposable? = null
     private val interactor by inject<PriceListInteractor>()
 
-    fun onStartPointTextChanged(startPointSource: String) {
-
-        startPointObservable = Observable.just(startPointSource)
-            .debounce(1000, TimeUnit.MILLISECONDS)
-            .distinctUntilChanged()
-            .filter { startPointSource.isNotBlank() }
-            .flatMap { interactor.findStartPlaces(it) }
-            .subscribe {
-                    it -> println("test " + it)
-            }
-
+    fun onStartPointTextChanged(startPointSource: String): Observable<MutableList<PlacesModel>> {
+        println()
+        return interactor.findStartPlaces(startPointSource)
     }
 
 }
